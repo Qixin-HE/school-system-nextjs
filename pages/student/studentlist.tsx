7c4d391f6be4ef41fcb2d645c2b7cf36dcfa7012
QIXIN HE <qhee0011@student.monash.edu>
2021-12-04 19:05:20 +1100

    Add button added

--- pages/student/studentlist.tsx
+++ pages/student/studentlist.tsx
@@ -136,6 +136,76 @@ const StudentListPage = () => {
                     <Breadcrumb.Item>Student</Breadcrumb.Item>
                     <Breadcrumb.Item>Student List</Breadcrumb.Item>
                 </Breadcrumb>
+                <Button key="add" type="primary" onClick={showModal} style={{marginBottom: "10px"}}>
+                                            Add
+                                        </Button>
+                <Modal title="Add Student" mask={false} visible={isModalVisible} onOk={handleOk}
+                                    onCancel={handleCancel} footer={[
+                                        <Button key="add" type="primary" onClick={handleOk}>
+                                            Add
+                                        </Button>,
+                                        <Button key="cancel" onClick={handleCancel}>
+                                            Cancel
+                                        </Button>,
+
+                                    ]}>
+                                    <Form {...layout} form={form} name="control-hooks">
+                                        <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+                                            <Input onChange={e => {
+                                                form.setFieldsValue({ name: e.target.value });
+                                                setName(e.target.value)
+                                            }} />
+                                        </Form.Item>
+                                        <Form.Item name="email" label="Email" rules={[{ type: 'email', required: true }]}>
+                                            <Input onChange={e => {
+                                                form.setFieldsValue({ email: e.target.value })
+                                                setEmail(e.target.value)
+                                            }} />
+                                        </Form.Item>
+                                        <Form.Item name="area" label="Area" rules={[{ required: true }]}>
+                                            <Select
+                                                placeholder="Select an option"
+                                                onChange={onAreaChange}
+                                                allowClear
+                                            >
+                                                <Option value="china">China</Option>
+                                                <Option value="zimbabwe">Zimbabwe</Option>
+                                                <Option value="lebanon">Lebanon</Option>
+                                                <Option value="other">Other</Option>
+                                            </Select>
+                                        </Form.Item>
+                                        <Form.Item
+                                            noStyle
+                                            shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+                                        >
+                                            {({ getFieldValue }) =>
+                                                getFieldValue('area') === 'other' ? (
+                                                    <Form.Item name="customizeArea" label="Customize Area" rules={[{ required: true }]}>
+                                                        <Input />
+                                                    </Form.Item>
+                                                ) : null
+                                            }
+                                        </Form.Item>
+                                        <Form.Item name="studenttype" label="Student Type" rules={[{ required: true }]}>
+                                            <Select
+                                                placeholder="Select an option."
+                                                onChange={onStudentTypeChange}
+                                                allowClear
+                                            >
+                                                <Option value="developer">Developer</Option>
+                                                <Option value="tester">Tester</Option>
+
+                                            </Select>
+                                        </Form.Item>
+                                        <Form.Item {...tailLayout}>
+
+
+                                            <Button type="link" htmlType="button" onClick={onFill}>
+                                                Fill form
+                                            </Button>
+                                        </Form.Item>
+                                    </Form>
+                                </Modal>
                 <Table dataSource={data} pagination={pagination} onChange={handleTableChange} >
                     <Column title="No." dataIndex="id" key="id" />
                     <Column title="Name" dataIndex="name" key="name" />
